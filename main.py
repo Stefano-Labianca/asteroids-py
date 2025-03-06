35e3fd9df74834c60d3fc54eb9373751945f4149
Stefano Labianca <lstefano@proton.me>
2025-03-06 15:00:57 +0100

    Created the player and show it on the screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
@@ -11,6 +12,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -19,8 +21,10 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
-        pygame.display.flip()
 
+        player.draw(screen)
+
+        pygame.display.flip()
         dt = clock.tick(60) / 1000
 
 
