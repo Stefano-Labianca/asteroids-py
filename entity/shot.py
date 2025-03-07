4a4ce44334502d99b6488fdabc84ec70f2370c12
Stefano Labianca <lstefano@proton.me>
2025-03-07 19:30:49 +0100

    Added TTL for a bullet

--- entity/shot.py
+++ entity/shot.py
@@ -9,10 +9,16 @@ class Shot(CircleShape):
 
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
+        self.__time_to_live = 0.7
 
     def draw(self, screen):
         pygame.draw.circle(screen, (255, 255, 255),
                            self.position, self.radius, 2)
 
     def update(self, dt):
+        self.__time_to_live -= dt
+
+        if self.__time_to_live <= 0:
+            self.kill()
+
         self.position += self.velocity * dt
